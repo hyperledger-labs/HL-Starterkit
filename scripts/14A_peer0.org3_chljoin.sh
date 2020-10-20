205523d9cb039464bef7dad8e755f28cabbc6267
ravinayag <ravinayag@gmail.com>
2020-10-20 23:23:06 +0530

    missing dirs
    Signed-off-by: ravinayag <ravinayag@gmail.com>

---
+++ scripts/14A_peer0.org3_chljoin.sh
@@ -0,0 +1,14 @@
+source scripts/.c.env
+echo -e $PCOLOR"Joining the {PEER_NAME0}.{ORG_3} to {CHANNEL_NAME1} ..."$NONE
+#set -x
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/{ORG_3}.{DOMAIN_NAME}/peers/{PEER_NAME0}.{ORG_3}.{DOMAIN_NAME}/tls/ca.crt
+CORE_PEER_TLS_KEY_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/{ORG_1}.{DOMAIN_NAME}/peers/{PEER_NAME0}.{ORG_1}.{DOMAIN_NAME}/tls/server.key
+CORE_PEER_LOCALMSPID={ORG_3_C}MSP
+CORE_VM_ENDPOINT=unix:///host/var/run/docker.sock
+CORE_PEER_TLS_CERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/{ORG_1}.{DOMAIN_NAME}/peers/{PEER_NAME0}.{ORG_1}.{DOMAIN_NAME}/tls/server.crt
+CORE_PEER_TLS_ENABLED=true
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/{ORG_3}.{DOMAIN_NAME}/users/Admin@{ORG_3}.{DOMAIN_NAME}/msp
+CORE_PEER_ID={CLI_NAME}
+CORE_PEER_ADDRESS={PEER_NAME0}.{ORG_3}.{DOMAIN_NAME}:11051
+#CHANNEL_NAME=mychannel2
+peer channel join -b {CHANNEL_NAME1}.block
