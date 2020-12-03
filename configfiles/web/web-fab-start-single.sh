a60563be5c155c5d8096a099ff1210e1fcde71b7
ravinayag <ravinayag@gmail.com>
2020-12-03 20:56:02 +0530

    singlehost error corrections
    Signed-off-by: ravinayag <ravinayag@gmail.com>

--- configfiles/web/web-fab-start-single.sh
+++ configfiles/web/web-fab-start-single.sh
@@ -2,27 +2,25 @@ source .env
 source .hlc.env
 source .c.env
 
-export `cat .hlc.env | grep IMAGE_TAG`
 
 echo "Ensure at this time you downloaded fabric-samples, binaries and Fabric-images, 
-  If not please download the prereq script and  run "FbinImage" from your home folder."
-
+      If not please download the prereq script and  run "FbinImage" from your home folder."
+echo " Note : If fabric version 1.4.3 and couchDB version is 0.4.15, then please run this command : DBimagechg"
+echo  " Execute with below commands in sequence"
+echo -e $GRCOLOR "ex :  \n DBimagechg(optional) \n shfstartfabnet \n shfCHLcreate \n shfPeerConnect  \n shfexplorer \n shfstop" $NONE
 
 
 cd $HL_CFG_PATH/
-
 cp -r ../asset-transfer* ../chaincode/    ## Copying sample to chaincode folder
+export `cat .hlc.env | grep IMAGE_TAG`
 
 
+function shfreplKeys () {
 
-function replKeys () {
-
-    source .hlc.env
     echo "running keys replacements"
-    if [ $HLENV != "WEB" ];then 
+    
     ORG1KEY="$(ls crypto-config/peerOrganizations/$ORG_1.$DOMAIN_NAME/ca/ | grep 'sk$')"
     sed -i -e "s/{ORG1-CA-KEY}/$ORG1KEY/g" ./base/ca-base.yaml
-
 
     ORG1TLSKEY="$(ls crypto-config/peerOrganizations/$ORG_1.$DOMAIN_NAME/tlsca/ | grep 'sk$')"
     sed -i -e "s/{ORG1-TLSCA-KEY}/$ORG1TLSKEY/g" ./base/ca-base.yaml
@@ -30,74 +28,81 @@ function replKeys () {
     ORG2KEY="$(ls crypto-config/peerOrganizations/$ORG_2.$DOMAIN_NAME/ca/ | grep 'sk$')"
     sed -i -e "s/{ORG2-CA-KEY}/$ORG2KEY/g" ./base/ca-base.yaml
     
-
     ORG2TLSKEY="$(ls crypto-config/peerOrganizations/$ORG_2.$DOMAIN_NAME/tlsca/ | grep 'sk$')"
     sed -i -e "s/{ORG2-TLSCA-KEY}/$ORG2TLSKEY/g" ./base/ca-base.yaml
+
+}
+
+function shfstartfabnet () {
+    
+    if [ "$ORD_TYPE" == "SOLO"  ]; then
+      echo "running ./scripts/1a_firsttimeonly.sh"
+      rm -rf crypto-config
+      mkdir -p channel-artifacts
+      ./scripts/1a_firsttimeonly.sh
+      shfreplKeys
+      echo -e $GRCOLOR" `figlet Starting Fabric`"$NONE
+      echo -e $GRCOLO" Starting with SOLO Orderer "$NONE
+      docker-compose -f docker-compose-cli.yaml up -d
+      sleep 15 
+    elif [ "$ORD_TYPE" == "RAFT" ]; then
+      echo "running ./scripts/1a_firsttimeonly.sh"
+      rm -rf crypto-config
+      mkdir -p channel-artifacts
+      ./scripts/1a_firsttimeonly.sh
+      shfreplKeys
+      echo -e $GRCOLOR" `figlet Starting Fabric`"$NONE
+      echo -e $GRCOLO" Starting with RAFT Orderer "$NONE
+      docker-compose -f docker-compose-cli.yaml -f docker-compose-orderer-etcraft.yaml up -d
+      sleep 15 
     else echo ""
     fi
+    sleep 5 
+    docker ps -a
+
+}
+
+function shfstop () {
+  docker-compose -f docker-compose-cli.yaml -f docker-compose-orderer-etcraft.yaml down -v 
+  docker-compose -f explorer/docker-compose-explorer.yml down -v
+  docker stop $(docker ps -qa)
+  docker image rm $(docker image ls | grep -E 'mycc|dev' | awk '{print $3}')
+  docker volume prune -f
+  docker network prune -f
+  docker ps -a
+  docker image ls  | grep -E 'mycc|dev'
+}
+
+function shfCHLcreate () {
+  
+  export CLI_NAME="$(docker ps --format='{{.Names}}' | grep cli)"
+  echo "CLI_NAME=$CLI_NAME" >> .hlc.env
+  ./scripts/2_channelcrte.sh
+
 }
 
 
-echo -e $GRCOLOR" `figlet Starting Fabric`"$NONE
-if [ "$ORD_TYPE" == "SOLO"  ]; then
-  echo running ./scripts/1a_firsttimeonly.sh
-  rm -rf crypto-config
-  mkdir -p channel-artifacts
-  ./scripts/1a_firsttimeonly.sh
-  replKeys
-  echo -e $GRCOLO" Starting with SOLO Orderer "$NONE
-  docker-compose -f docker-compose-cli.yaml up -d
-  sleep 5 
-elif [ "$ORD_TYPE" == "RAFT" ]; then
-  echo -e $GRCOLO" Starting with RAFT Orderer "$NONE
-  echo running ./scripts/1a_firsttimeonly.sh
-  rm -rf crypto-config
-  mkdir -p channel-artifacts
-  ./scripts/1a_firsttimeonly.sh
-  replKeys
-  docker-compose -f docker-compose-cli.yaml -f docker-compose-orderer-etcraft.yaml up -d
-  sleep 15 
-else echo ""
-fi
-sleep 10 
 
-docker ps -a
-export CLI_NAME="$(docker ps --format='{{.Names}}' | grep cli)"
-echo "CLI_NAME=$CLI_NAME" >> .hlc.env
-./scripts/2_channelcrte.sh
-
-
-function peernetconnect() {
-    source .hlc.env
+function shfPeerConnect () {
     
-
     sleep 1
     docker exec $CLI_NAME bash ./scripts/2a_peer0.org1_chljoin.sh
     sleep 3
     
-
     docker exec $CLI_NAME bash ./scripts/3_peer1.org1_chljoin.sh
     sleep 2
-
 
     docker exec $CLI_NAME bash ./scripts/4_peer0.org2_chljoin.sh
     sleep 1
   
-
     docker exec $CLI_NAME bash ./scripts/5_peer1.org2_chljoin.sh
     sleep 1
-
-
 
     docker exec $CLI_NAME bash ./scripts/6_anchorpeerorg1.sh
     sleep 2
 
-
-
     docker exec $CLI_NAME bash ./scripts/7_anchorpeerorg2.sh
     sleep 2
-
-
 
     docker exec $CLI_NAME bash ./scripts/8_ccinstallpeer0.org1.sh
     sleep 2
@@ -105,14 +110,12 @@ function peernetconnect() {
     docker exec $CLI_NAME bash ./scripts/9_ccinstallpeer0.org2.sh
     sleep 2
     
-
     if [[ $IMAGE_TAG != @(2.0.0|2.1.0|2.2.0) ]];
     then 
         docker tag hyperledger/fabric-ccenv:1.4.3 hyperledger/fabric-ccenv:latest   #Bug Fix
 
         docker exec $CLI_NAME bash ./scripts/10_ccinstantiate.org2.sh
         sleep 2
-
 
         docker exec $CLI_NAME bash ./scripts/11_ccquery.org1.sh
         sleep 1
@@ -124,13 +127,33 @@ function peernetconnect() {
     else 
         echo "...skiping to fab 2.0"
         docker tag hyperledger/fabric-ccenv:2.2 hyperledger/fabric-ccenv:latest    ## Bug fix in 2.2
+        sleep 2
         docker exec $CLI_NAME bash ./scripts/9a_lcccapprovefab2.0.sh
-
+        sleep 2
         docker exec $CLI_NAME bash ./scripts/10a_lccc-commitfab2.0.sh
-
 
     fi
     echo ">>>>>  Peerconnect done with two org setup   <<<<< "
 }
 
-peernetconnect
+function DBimagechg (){
+    source .env
+    #source .hlc.env
+    export `cat .hlc.env | grep DBIMAGE_TAG`
+    if [ $DBIMAGE_TAG == 0.4.15 ];then
+    docker pull hyperledger/fabric-couchdb:0.4.15
+    docker tag hyperledger/fabric-couchdb:0.4.15 couchdb:0.4.15
+    #sed -i -e "s/COUCHDB_USER=admin/COUCHDB_USER=/g" $HL_CFG_PATH/docker-compose-cli.yaml
+    #sed -i -e "s/COUCHDB_PASSWORD=password/COUCHDB_PASSWORD=/g" $HL_CFG_PATH/docker-compose-cli.yaml
+    fi
+}
+
+
+function shfexplorer (){
+    
+    cp -r crypto-config/*  explorer/examples/net1/crypto/
+    EXPORG1KEY="$(ls crypto-config/peerOrganizations/$ORG_1.$DOMAIN_NAME/users/Admin@$ORG_1.$DOMAIN_NAME/msp/keystore/ | grep 'sk$')"
+    sed -i -e "s/{ORG1-MSP-KEY}/$EXPORG1KEY/g" ./explorer/examples/net1/connection-profile/first-network.json
+    docker-compose -f explorer/docker-compose-explorer.yml up -d
+
+}
--- hlstartup/08_expstart.sh
+++ hlstartup/08_expstart.sh
@@ -10,6 +10,7 @@ export COMPOSE_PROJECT_NAME=automation
 function explorerConfig() {
     cp ./configfiles/explorer/docker-compose-explorer-org.yml ./explorer/docker-compose-explorer.yml
     cp ./configfiles/explorer/first-network-org.json  ./explorer/examples/net1/connection-profile/first-network.json
+    cp ./configfiles/explorer/config-org.json  ./explorer/examples/net1/config.json
 
     sed -i -e "s/{DOMAIN_NAME}/$DOMAIN_NAME/g" ./explorer/docker-compose-explorer.yml
 
@@ -18,26 +19,31 @@ function explorerConfig() {
     sed -i -e "s/{ORG_1_C}/$ORG_1_C/g" ./explorer/examples/net1/connection-profile/first-network.json
     sed -i -e "s/{ORG_1}/$ORG_1/g" ./explorer/examples/net1/connection-profile/first-network.json
     sed -i -e "s/{CHANNEL_NAME1}/$CHANNEL_NAME1/g" ./explorer/examples/net1/connection-profile/first-network.json
+    sed -i -e "s/{CA_ORG1}/$CA_ORG1/g" ./explorer/examples/net1/connection-profile/first-network.json
 
-
+    if [ $HLENV != "WEB" ];then
     EXPORG1KEY="$(ls crypto-config/peerOrganizations/$ORG_1.$DOMAIN_NAME/users/Admin@$ORG_1.$DOMAIN_NAME/msp/keystore/ | grep 'sk$')"
     sed -i -e "s/{ORG1-MSP-KEY}/$EXPORG1KEY/g" ./explorer/examples/net1/connection-profile/first-network.json
+    else echo ""
+    fi
 }
 
 function startexplore() { 
+    
+    mkdir -p ./explorer/examples/net1/crypto
+    if [ $HLENV != "WEB" ];then
     echo -e $GRCOLOR" `figlet starting explorer`"$NONE
-
     rm -rf ./explorer/examples/net1/crypto/*
     cp -r ./crypto-config/* ./explorer/examples/net1/crypto/
-
-    cd explorer && docker-compose -f docker-compose-explorer.yml up -d
+    docker-compose -f explorer/docker-compose-explorer.yml up -d
     pwd
     docker ps -a  | grep explorer
     echo
     echo
     echo -e $GRCOLOR " Docker explorer can access from http://yourserverip:8090 "$NONE
     echo
-
+    else echo ""
+    fi
 }
 
 
