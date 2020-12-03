262658245a5c8758a456539f8046502d62d4f6af
ravinayag <ravinayag@gmail.com>
2020-12-03 13:18:02 +0530

    minor error
    Signed-off-by: ravinayag <ravinayag@gmail.com>

--- configfiles/k8s/k8scripts/04_invoke.sh
+++ configfiles/k8s/k8scripts/04_invoke.sh
@@ -1,37 +1,44 @@
 source k8scripts/.hlc.env
+source k8scripts/.c.env
+
 if [ -z $SAMPLE_CC ] ; then
 export CC_LABLNAME="marbles"
 else
 export CC_LABLNAME="${SAMPLE_CC,,}"
 fi
-echo  "  Initiating ......"
-
+echo -e $PCOLOR "  Initiating ......" $NONE
 
 echo $CC_LABLNAME
 
 export PEER_CONN_PARMS="--peerAddresses {PEER_NAME0}-{ORG_1}:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/{ORG_1}/peers/{PEER_NAME0}-{ORG_1}/tls/ca.crt"
 export PEER_CONN_PARMS1="--peerAddresses {PEER_NAME0}-{ORG_2}:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/{ORG_2}/peers/{PEER_NAME0}-{ORG_2}/tls/ca.crt"
 sleep 5
-echo -e $PCOLOR"Initiating by marbles {ORG_1} "$NONE
+echo -e $PCOLOR" Initiating by marbles {ORG_1} "$NONE
 peer chaincode invoke -o {ORD_NAME0}:7050 --isInit --tls true --cafile $ORDERER_CA -C {CHANNEL_NAME1}  -n  $CC_LABLNAME $PEER_CONN_PARMS $PEER_CONN_PARMS1 -c '{"Args":["initMarble","marble1","blue","35","tom"]}'
 sleep 5
-echo -e $PCOLOR"Invoking for marbles -{ORG_1} "$NONE
+echo -e $PCOLOR" Invoking for marbles -{ORG_1} "$NONE
+
+echo ""
 peer chaincode invoke -o {ORD_NAME0}:7050 --tls true --cafile $ORDERER_CA -C {CHANNEL_NAME1}  -n  $CC_LABLNAME $PEER_CONN_PARMS $PEER_CONN_PARMS1 -c '{"Args":["initMarble","marble1","blue","35","tom"]}'
 echo ""
 sleep 5
+echo -e $PCOLOR "  Querying the asset from {ORG_1}"$NONE
+echo ""
+peer chaincode query -C {CHANNEL_NAME1} -n  $CC_LABLNAME -c '{"Args":["readMarble","marble1"]}'
+
+
+
+#### FABCAR TASKS #####
 #echo -e $PCOLOR"Invoke transaction by creating a new FABCAR from {ORG_1} "$NONE
 #peer chaincode invoke -o {ORD_NAME0}:7050 --tls --cafile $ORDERER_CA -C {CHANNEL_NAME1} -n $CC_LABLNAME $PEER_CONN_PARMS $PEER_CONN_PARMS1 -c  '{"function": "createCar","Args":["Battery-Car", "Tesla", "A100", "Bright-Red", "vinayag"]}'
 
-echo ""
-#echo  "  Querying all asset "
-echo ""
-sleep 5
-#peer chaincode query -C {CHANNEL_NAME1} -n  $CC_LABLNAME -c '{"Args":["queryCar","CAR0"]}' 
-echo ""
+# echo ""
+# #echo  "  Querying all asset "
+# echo ""
+# sleep 5
+# #peer chaincode query -C {CHANNEL_NAME1} -n  $CC_LABLNAME -c '{"Args":["queryCar","CAR0"]}' 
+# echo ""
 
-sleep 3
-echo " Querying the asset from {ORG_1}"
-echo ""
-peer chaincode query -C {CHANNEL_NAME1} -n  $CC_LABLNAME -c '{"Args":["readMarble","marble1"]}'
+# sleep 3
 
 
--- configfiles/k8s/k8scripts/05_query.sh
+++ configfiles/k8s/k8scripts/05_query.sh
@@ -8,27 +8,29 @@ export CC_LABLNAME="${SAMPLE_CC,,}"
 fi
 export PEER_CONN_PARMS="--peerAddresses {PEER_NAME0}-{ORG_1}:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/{ORG_1}/peers/{PEER_NAME0}-{ORG_1}/tls/ca.crt"
 export PEER_CONN_PARMS1="--peerAddresses {PEER_NAME0}-{ORG_2}:7051 --tlsRootCertFiles /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/{ORG_2}/peers/{PEER_NAME0}-{ORG_2}/tls/ca.crt"
-sleep 5
+sleep 3
 
 echo ""
-echo  "  Querying the asset from {ORG_2} "
+echo  -e $PCOLOR "  Querying specific transaction asset from {ORG_2} "$NONE
 echo ""
-sleep 5
-#peer chaincode query -C {CHANNEL_NAME1} -n  $CC_LABLNAME -c '{"Args":["queryCar","CAR0"]}' 
+
 peer chaincode query -C {CHANNEL_NAME1} -n  $CC_LABLNAME -c '{"Args":["readMarble","marble1"]}'
 sleep 2
-#echo -e $PCOLOR"Querying specific transaction on invoke {ORG_2} "$NONE
-#peer chaincode query -C {CHANNEL_NAME1} -n  $CC_LABLNAME -c '{"Args":["readMarble","marble1"]}'
+
 echo ""
 sleep 3
 echo -e $PCOLOR" Second invoke ..... from {ORG_2}, Invoke transaction by creating a new Marble "$NONE
 echo ""
 peer chaincode invoke -o {ORD_NAME0}:7050  --tls true --cafile $ORDERER_CA -C {CHANNEL_NAME1} -n  $CC_LABLNAME  $PEER_CONN_PARMS $PEER_CONN_PARMS1  -c '{"Args":["initMarble","marble2","pink","1000","Jerry"]}' --waitForEvent
 
-sleep 4
-echo " Querying the asset from {ORG_2}"
+sleep 5
+echo -e $PCOLOR " Querying the asset from {ORG_2}" $NONE
 echo ""
 peer chaincode query -C {CHANNEL_NAME1} -n  $CC_LABLNAME -c '{"Args":["readMarble","marble2"]}'
 sleep 2
-#peer chaincode query -C {CHANNEL_NAME1} -n  $CC_LABLNAME -c '{"Args":["readMarble","Battery-Car"]}'
 
+
+
+#### FABCAR TASKS #####
+#peer chaincode query -C {CHANNEL_NAME1} -n  $CC_LABLNAME -c '{"Args":["queryCar","CAR0"]}' 
+#peer chaincode query -C {CHANNEL_NAME1} -n  $CC_LABLNAME -c '{"Args":["readMarble","Battery-Car"]}'
